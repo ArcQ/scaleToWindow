fad5d619abefa57f587258cd2b2417ad8ce9e7d3
ArcQ <eddielaw296@gmail.com>
2017-11-08 22:04:16 -0500

    feat: update screen resolutions for screens like mobile safari

--- scaleToWindow.js
+++ scaleToWindow.js
@@ -1,10 +1,19 @@
-function scaleToWindow(canvas, backgroundColor) {
+function scaleToWindow(canvas, backgroundColor, _window, _document) {
   var scaleX, scaleY, scale, center;
+
+  const getWWidth = () => ((iOS) ? _window.screen.width : _getWWidth()
+    || _document._documentElement.clientWidth
+    || _document.body.clientWidth);
+
+  const getWHeight = () => ((iOS) ? _window.screen.height : _getWHeight()
+    || _document._documentElement.clientHeight
+    || _document.body.clientHeight);
+
 
   //1. Scale the canvas to the correct size
   //Figure out the scale amount on each axis
-  scaleX = window.innerWidth / canvas.offsetWidth;
-  scaleY = window.innerHeight / canvas.offsetHeight;
+  scaleX = getWWidth() / canvas.offsetWidth;
+  scaleY = getWHeight() / canvas.offsetHeight;
 
   //Scale the canvas based on whichever value is less: `scaleX` or `scaleY`
   scale = Math.min(scaleX, scaleY);
@@ -13,16 +22,16 @@ function scaleToWindow(canvas, backgroundColor) {
 
   //2. Center the canvas.
   //Decide whether to center the canvas vertically or horizontally.
-  //Wide canvases should be centered vertically, and 
+  //Wide canvases should be centered vertically, and
   //square or tall canvases should be centered horizontally
   if (canvas.offsetWidth > canvas.offsetHeight) {
-    if (canvas.offsetWidth * scale < window.innerWidth) {
+    if (canvas.offsetWidth * scale < getWWidth()) {
       center = "horizontally";
     } else {
       center = "vertically";
     }
   } else {
-    if (canvas.offsetHeight * scale < window.innerHeight) {
+    if (canvas.offsetHeight * scale < getWHeight()) {
       center = "vertically";
     } else {
       center = "horizontally";
@@ -32,16 +41,16 @@ function scaleToWindow(canvas, backgroundColor) {
   //Center horizontally (for square or tall canvases)
   var margin;
   if (center === "horizontally") {
-    margin = (window.innerWidth - canvas.offsetWidth * scale) / 2;
+    margin = (getWWidth() - canvas.offsetWidth * scale) / 2;
     canvas.style.marginTop = 0 + "px";
     canvas.style.marginBottom = 0 + "px";
     canvas.style.marginLeft = margin + "px";
     canvas.style.marginRight = margin + "px";
   }
 
-  //Center vertically (for wide canvases) 
+  //Center vertically (for wide canvases)
   if (center === "vertically") {
-    margin = (window.innerHeight - canvas.offsetHeight * scale) / 2;
+    margin = (getWHeight() - canvas.offsetHeight * scale) / 2;
     canvas.style.marginTop = margin + "px";
     canvas.style.marginBottom = margin + "px";
     canvas.style.marginLeft = 0 + "px";
@@ -57,7 +66,7 @@ function scaleToWindow(canvas, backgroundColor) {
   canvas.style.display = "block";
 
   //4. Set the color of the HTML body background
-  document.body.style.backgroundColor = backgroundColor;
+  _document.body.style.backgroundColor = backgroundColor;
 
   //Fix some quirkiness in scaling for Safari
   var ua = navigator.userAgent.toLowerCase();
@@ -71,7 +80,7 @@ function scaleToWindow(canvas, backgroundColor) {
     }
   }
 
-  //5. Return the `scale` value. This is important, because you'll nee this value 
+  //5. Return the `scale` value. This is important, because you'll nee this value
   //for correct hit testing between the pointer and sprites
   return scale;
 }
